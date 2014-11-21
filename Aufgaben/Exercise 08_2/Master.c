c74141819687b006d49113a4a8730f341a4d83ef
Andreas Rubik <rubikandreas@gmail.com>
2014-11-21 14:32:22 +0100

    added Exercise 08_2

---
+++ Aufgaben/Exercise 08_2/Master.c
@@ -0,0 +1,16 @@
+/* Mastervorlage Gueltig ab: 2011-07-30*/
+
+/* Folgende Angaben sind hier im Kommentar verpflichtend zu erfassen */
+/* Andreas Rubik */
+/* 1410258038  */
+/* 08_1 */ 
+
+/* unsafe warning wird ausgeblendet */
+#define _CRT_SECURE_NO_WARNINGS 
+
+/* Programmcode hier einfügen */
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+
+}
