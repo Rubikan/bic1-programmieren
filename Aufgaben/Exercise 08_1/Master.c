a7456ac23c9d8f6ef8cc7e0c801b16bc00997711
Andreas Rubik <rubikandreas@gmail.com>
2014-11-25 16:12:33 +0100

    changed letter

--- Aufgaben/Exercise 08_1/Master.c
+++ Aufgaben/Exercise 08_1/Master.c
@@ -17,7 +17,7 @@ int main(int argc, char* argv[]) {
 	double b = 10;
 	double c = 15;
 
-	printf("Das Volumen eines Wuerfels mit Kantenlänge %.2f = %.2f.\n", a, volume_cube(a));
+	printf("Das Volumen eines Wuerfels mit Kantenlaenge %.2f = %.2f.\n", a, volume_cube(a));
 	printf("Das Volumen eines Quaders mit a = %.2f, b = %.2f und c = %.2f ist %.2f.\n", a, b, c, volume_cuboid(a, b, c));
 	printf("Die Oberflaeche eines Quaders mit a = %.2f, b = %.2f und c = %.2f ist %.2f.\n", a, b, c, surface_cuboid(a, b, c));
 	printf("Die Zahl PI = %f\n", PI);
